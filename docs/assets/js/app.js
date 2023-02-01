6b6567294714e921dd46480d51c68ad59c295386
Derristy <derristy@gmail.com>
2023-02-02 00:05:42 +0200

    multiple dropdown

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,16 +1,25 @@
 let dropdown = document.querySelectorAll(".dropdown");
 
 dropdown.forEach((element) => {
-    let dropdownBtn = element.querySelector(".dropdown-btn");
-
     element.addEventListener("click", (event) => {
-        element.classList.toggle("show");
+        event.stopPropagation();
         event.preventDefault();
+        element.classList.toggle("show");
 
         document.querySelectorAll(".dropdown.show").forEach((element) => {
             if(element != event.target.closest(".dropdown.show")){
                 element.classList.remove("show");
             }
         });
+    });
+
+    element.querySelector(".dropdown-menu").addEventListener("click", (event) => {
+        event.stopPropagation();
+    });
+});
+
+window.addEventListener("click", () => {
+    document.querySelectorAll(".dropdown.show").forEach((element) => {
+        element.classList.remove("show");
     });
 });
